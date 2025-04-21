d4006f52a257d8cadd8807fa1bdd2e25f4e6ad5f
prasaddharani <dharaniprasad1999@gmail.com>
2025-04-21 09:03:10 +0530

    Dutch National Flag Algorithm

--- src/main/leetcode/SortingProblems.java
+++ src/main/leetcode/SortingProblems.java
@@ -223,4 +223,35 @@ public class SortingProblems {
 
         return dummy.next;
     }
+
+    /* Dutch National Flag algorithm */
+
+    /*
+    75. Sort Colors
+     */
+
+    /*
+    Input: nums = [2,0,2,1,1,0]
+    Output: [0,0,1,1,2,2]
+     */
+
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
 }
