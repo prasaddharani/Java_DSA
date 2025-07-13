69d0dc70c5602759e06ef2abde24ba57a45d8481
prasaddharani <dharaniprasad1999@gmail.com>
2025-07-13 17:54:30 +0530

    Different types of injections

---
+++ src/main/java/org/example/spring/basic/DependencyInjection.java
@@ -0,0 +1,85 @@
+package org.example.spring.basic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+// Engine Bean
+@Component
+class Engine {
+    public String start() {
+        return "Engine started";
+    }
+}
+
+// Car Bean with field injection
+@Component
+class Car {
+    @Autowired
+    private Engine engine;
+
+    public void drive() {
+        System.out.println(engine.start() + " and car is moving through FieldInjection");
+    }
+}
+
+// Audi Car with Setter injection
+@Component
+class SetterInjectionCar {
+    private Engine engine;
+
+    @Autowired
+    private void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void drive() {
+        System.out.println(engine.start() + " and car is moving through SetterInjection");
+    }
+}
+
+@Component
+class ConstructorInjectionCar {
+    private final Engine engine;
+
+    @Autowired  // Optional in Spring 4.3+ for single constructor
+    public ConstructorInjectionCar(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void drive() {
+        System.out.println(engine.start() + " and car is moving through ConstructorInjection");
+    }
+}
+
+
+// Spring Configuration Class
+@Configuration
+@ComponentScan("org.example.spring.basic")
+class AppConfig {
+}
+
+// Main Class to run the demo
+public class DependencyInjection {
+    public static void main(String[] args) {
+        // Create Spring container
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // Get Car bean
+        Car car = context.getBean(Car.class);
+
+        SetterInjectionCar setterInjectionCar = context.getBean(SetterInjectionCar.class);
+
+        ConstructorInjectionCar constructorInjectionCar = context.getBean(ConstructorInjectionCar.class);
+
+        // Run the method
+        car.drive();
+        setterInjectionCar.drive();
+        constructorInjectionCar.drive();
+
+        // Close context
+        context.close();
+    }
+}
