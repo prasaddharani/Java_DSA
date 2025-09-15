80693c03fb9dc008c9c8bc164b5fd7b8c5657ee5
prasaddharani <dharaniprasad1999@gmail.com>
2025-09-15 09:15:58 +0530

    Sorting

--- src/main/java/org/example/leetcode/Blind75.java
+++ src/main/java/org/example/leetcode/Blind75.java
@@ -985,5 +985,37 @@ class QueueProblem {
     }
 }
 
+class Sorting {
+
+    /*
+    Input: nums = [2,0,2,1,1,0]
+    Output: [0,0,1,1,2,2]
+     */
+    public int[] sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[mid] ;
+                nums[mid] = nums[low];
+                nums[low] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 2) {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            } else {
+                mid++;
+            }
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        Sorting sorting = new Sorting();
+        System.out.println(Arrays.toString(sorting.sortColors(new int[]{2, 0, 2, 1, 1, 0})));
+    }
+}
 public class Blind75 {
 }
