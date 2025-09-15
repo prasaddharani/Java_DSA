a5c63cd9fa69ed6038ed03cda84aae6b8abf8fe1
prasaddharani <dharaniprasad1999@gmail.com>
2025-09-15 11:18:10 +0530

    Binary search problems

--- src/main/java/org/example/leetcode/Blind75.java
+++ src/main/java/org/example/leetcode/Blind75.java
@@ -1087,10 +1087,53 @@ class BinarySearchProblem {
         return binarySearchHelper(nums, target, 0, nums.length - 1);
     }
 
+    /*
+    Input: nums1 = [1,3], nums2 = [2]
+    Output: 2.00000
+    Explanation: merged array = [1,2,3] and median is 2.
+     */
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int total = (nums1.length + nums2.length) / 2;
+        if (nums1.length > nums2.length) {
+            int[] temp = nums2;
+            nums2 = nums1;
+            nums1 = temp;
+        }
+        int x = nums1.length;
+        int y = nums2.length;
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = partitionX == 0 ? Integer.MIN_VALUE: nums1[partitionX - 1];
+            int minRightX = partitionX == x? Integer.MAX_VALUE: nums1[partitionX];
+
+            int maxLeftY = partitionY == 0 ? Integer.MIN_VALUE: nums2[partitionY - 1];
+            int minRightY = partitionY == y ? Integer.MAX_VALUE: nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if (total % 2 == 0) {
+                    return (double) (max(minRightX, minRightY) + min(maxLeftX, maxLeftY)) / 2;
+                } else {
+                    return max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY){
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         BinarySearchProblem binarySearchProblem = new BinarySearchProblem();
         //System.out.println(Arrays.toString(binarySearchProblem.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
-        System.out.println(binarySearchProblem.search(new int[]{4,5,6,7,0,1,2}, 0));
+        //System.out.println(binarySearchProblem.search(new int[]{4,5,6,7,0,1,2}, 0));
+        System.out.println(binarySearchProblem.findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
     }
 }
 public class Blind75 {
