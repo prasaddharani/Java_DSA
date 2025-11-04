83708fcf6129d62a6bb76784c882ab166321be4e
prasaddharani <dharaniprasad1999@gmail.com>
2025-11-04 15:33:53 +0530

    Thread creation in multiple different ways

---
+++ src/main/java/org/example/repractice/threads/basics/ThreadBasics.java
@@ -0,0 +1,49 @@
+package org.example.repractice.threads.basics;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+class ThreadCreate1 extends Thread {
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            log.info("Thread name: {} and processed index: {}", Thread.currentThread().getName(), i);
+        }
+    }
+}
+
+@Slf4j
+class ThreadCreate2 implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            log.info("Thread name: {} and processed index: {}", Thread.currentThread().getName(), i);
+        }
+    }
+}
+
+@Slf4j
+public class ThreadBasics {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new ThreadCreate1();
+        Thread t2 = new ThreadCreate1();
+        Runnable runnableThread = () -> {
+            for (int i = 0; i < 5; i++) {
+                log.info("Thread name: {} and processed index: {}", Thread.currentThread().getName(), i);
+            }
+        };
+        Thread t3 = new Thread(runnableThread);
+        Thread t4 = new Thread(new ThreadCreate2());
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+
+        try {
+            t4.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        log.info("Main Thread name: {}", Thread.currentThread().getName());
+    }
+}
