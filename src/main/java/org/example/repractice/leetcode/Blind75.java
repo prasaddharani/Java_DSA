d077243df46c06dc66b3df7c1cfe3f6e0d3ef4e2
prasaddharani <dharaniprasad1999@gmail.com>
2025-12-27 19:05:11 +0530

    Binary Search Problems

--- src/main/java/org/example/repractice/leetcode/Blind75.java
+++ src/main/java/org/example/repractice/leetcode/Blind75.java
@@ -809,9 +809,46 @@ class BinarySearchProblems {
         }
     }
 
+    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int total = nums1.length + nums2.length;
+
+        int x = nums1.length;
+        int y = nums2.length;
+
+        int low = 0;
+        int high = x;
+        while (low <= high) {
+            int partitionX = (low + high) / 2;
+            int partitionY = (x + y + 1) / 2 - partitionX;
+
+            int maxLeftX = partitionX == 0? Integer.MIN_VALUE: nums1[partitionX - 1];
+            int minRightX = partitionX == x? Integer.MAX_VALUE: nums1[partitionX];
+
+            int maxLeftY = partitionY == 0? Integer.MIN_VALUE: nums2[partitionY - 1];
+            int minRightY = partitionY == y? Integer.MAX_VALUE: nums2[partitionY];
+
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                if (total % 2 == 0) {
+                    return (double) (max(maxLeftX, maxLeftY) + min(minRightX, minRightY)) / 2;
+                } else {
+                    return max(maxLeftX, maxLeftY);
+                }
+            } else if (maxLeftX > minRightY) {
+                high = partitionX - 1;
+            } else {
+                low = partitionX + 1;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         //System.out.println(Arrays.toString(searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8)));
-        System.out.println(search(new int[]{4,5,6,7,0,1,2}, 0));
+        //System.out.println(search(new int[]{4,5,6,7,0,1,2}, 0));
+        System.out.println(findMedianSortedArrays(new int[]{1, 3}, new int[]{2}));
     }
 }
 
