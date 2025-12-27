09babd5a7394c6cfcea387cd08b7658978d39111
prasaddharani <dharaniprasad1999@gmail.com>
2025-12-27 17:35:47 +0530

    Dutch National Flag alg

--- src/main/java/org/example/repractice/leetcode/Blind75.java
+++ src/main/java/org/example/repractice/leetcode/Blind75.java
@@ -721,5 +721,34 @@ class QueueProblems {
     }
 }
 
+class DutchAlg {
+
+    public static int[] sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+
+        while (mid < high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[high];
+                nums[high] = nums[mid];
+                nums[mid] = temp;
+                high--;
+                mid++;
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(sortColors(new int[]{2, 0, 2, 1, 1, 0})));
+    }
+}
+
 public class Blind75 {
 }
