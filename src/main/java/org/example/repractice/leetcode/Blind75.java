af3bae528dd47996f544d00a15dc39f5c593ac96
prasaddharani <dharaniprasad1999@gmail.com>
2025-12-27 09:31:14 +0530

    HashTable based problems

--- src/main/java/org/example/repractice/leetcode/Blind75.java
+++ src/main/java/org/example/repractice/leetcode/Blind75.java
@@ -1,7 +1,10 @@
 package org.example.repractice.leetcode;
 
 
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.lang.Math.max;
 
 class BitManipulations {
 
@@ -41,6 +44,46 @@ class BitManipulations {
     public static void main(String[] args) {
         //System.out.println(Arrays.toString(countBits(5)));
         System.out.println(Arrays.toString(singleNumber(new int[]{1, 2, 1, 3, 2, 5})));
+    }
+}
+
+class HashTables {
+
+    private static String sortString(String str) {
+        char[] chars = str.toCharArray();
+        Arrays.sort(chars);
+        return Arrays.toString(chars);
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str: strs) {
+            String sorted = sortString(str);
+            map.computeIfAbsent(sorted, k -> new ArrayList<>()).add(str);
+        }
+        return map.values().stream().toList();
+    }
+
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> numSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
+        int length;
+        int maxLength = 0;
+        for (Integer num: nums) {
+            if (numSet.contains(num - 1)) {
+                continue;
+            }
+            length = 1;
+            while (numSet.contains(num + length)) {
+                length += 1;
+            }
+            maxLength = max(maxLength, length);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        //System.out.println(groupAnagrams(new String[]{"eat","tea","tan","ate","nat","bat"}));
+        System.out.println(longestConsecutive(new int[]{100,4,200,1,3,2}));
     }
 }
 public class Blind75 {
