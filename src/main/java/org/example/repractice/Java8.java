2fba16611d1d88bff551c54727553eed7bfefec8
prasaddharani <dharaniprasad1999@gmail.com>
2026-01-01 20:41:26 +0530

    Functional Interface: BiFunction

--- src/main/java/org/example/repractice/Java8.java
+++ src/main/java/org/example/repractice/Java8.java
@@ -1,10 +1,17 @@
 package org.example.repractice;
 
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Java8 {
     public static void main(String[] args) {
         Function<String, String> function = (a) -> "Hello " + a;
-        System.out.println(function.apply("Dharani"));
+        System.out.println("Function Example: " + function.apply("Dharani"));
+
+        BiFunction<Integer, Integer, Integer> biFunction = (a, b) -> a * b;
+        System.out.println("BiFunction Example: " + biFunction.apply(2, 3));
+
+
     }
 }
