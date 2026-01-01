f7d7dee5f2115499600f8218869ac148684459ba
prasaddharani <dharaniprasad1999@gmail.com>
2026-01-01 20:43:35 +0530

    Functional Interface: Consumer

--- src/main/java/org/example/repractice/Java8.java
+++ src/main/java/org/example/repractice/Java8.java
@@ -1,17 +1,21 @@
 package org.example.repractice;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+@Slf4j
 public class Java8 {
     public static void main(String[] args) {
         Function<String, String> function = (a) -> "Hello " + a;
-        System.out.println("Function Example: " + function.apply("Dharani"));
+        log.info("Function Example: {}", function.apply("Dharani"));
 
         BiFunction<Integer, Integer, Integer> biFunction = (a, b) -> a * b;
-        System.out.println("BiFunction Example: " + biFunction.apply(2, 3));
+        log.info("BiFunction Example: {}", biFunction.apply(2, 3));
 
-
+        Consumer<String> consumer = (a) -> log.info("Hello " + a + " From Consumer Example");
+        consumer.accept("Dharani");
     }
 }
