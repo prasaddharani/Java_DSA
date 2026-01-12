90ad6dcec491e9382980ad60fbe1bab40688e560
prasaddharani <dharaniprasad1999@gmail.com>
2026-01-12 08:09:19 +0530

    Sealed class example

---
+++ src/main/java/org/example/repractice/sealed/SealedClassDemo.java
@@ -0,0 +1,30 @@
+package org.example.repractice.sealed;
+
+sealed class Shape permits Circle, Rectangle {
+    public void printShape() {
+        System.out.println("Shape main class");
+    }
+}
+
+final class Rectangle extends Shape {
+
+    @Override
+    public void printShape() {
+        System.out.println("Rectangle shape");
+    }
+}
+
+final class Circle extends Shape {
+    @Override
+    public void printShape() {
+        System.out.println("Rectangle shape");
+    }
+}
+
+public class SealedClassDemo {
+
+    public static void main(String[] args) {
+        Shape rectangle = new Rectangle();
+        rectangle.printShape();
+    }
+}
